c72c6d57f23de6e9c0676d9b68d3c9f86e6fab1b
Rajeev Singh <rsinghh68@gmail.com>
2024-11-24 10:31:54 +0530

    add cors support
    - allow cors request from all domains as we don't have a fixed domain for frontend yet

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import prediction
 
-# Initialize the FastAPI app
 app = FastAPI()
 
-# Include the prediction route
+origins = [
+    "*",
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
+
 app.include_router(prediction.router)
 
 # Home route (optional)
